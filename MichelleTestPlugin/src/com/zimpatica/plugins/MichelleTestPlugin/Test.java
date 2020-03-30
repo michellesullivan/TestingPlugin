e8e08e83c93d306d192afa279f543b76a831bf5d
michellesullivan <michelle.sullivan@zimpatica.com>
2020-03-30 16:14:03 -0400

    Initial Upload

---
+++ MichelleTestPlugin/src/com/zimpatica/plugins/MichelleTestPlugin/Test.java
@@ -0,0 +1,14 @@
+package com.zimpatica.plugins.MichelleTestPlugin;
+
+import com.appiancorp.suiteapi.process.exceptions.SmartServiceException;
+import com.appiancorp.suiteapi.process.framework.AppianSmartService;
+
+public class Test extends AppianSmartService {
+
+	@Override
+	public void run() throws SmartServiceException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
